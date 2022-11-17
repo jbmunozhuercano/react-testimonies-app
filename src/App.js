ef777ff48f98a22488cefdecab1fe297c5179351
jbmunoz <jorge.munoz.huercano@gmail.com>
2022-11-17 12:12:06 +0100

    Added git status

--- src/App.js
+++ src/App.js
@@ -1,4 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+    const [testimonies, setTestimonies] = useState({});
+
+    async function getResponse() {
+        try {
+            const response = await axios.get(
+                "https://testimonialapi.toolcarton.com/api"
+            );
+            setTestimonies(response.data);
+            console.log(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getResponse();
+    }, []);
+
     return (
         <div className="App">
             <h1>Hello World!</h1>
