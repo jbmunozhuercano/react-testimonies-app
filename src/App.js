f3bf49fecd3ae9c87f3598aba19488d8cf1283c1
jbmunoz <jorge.munoz.huercano@gmail.com>
2022-11-17 13:24:33 +0100

    Added api url and sorted array of testimonies

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+import { TESTIMONIES_API_URL } from "./api";
+
 function App() {
     const [testimonies, setTestimonies] = useState();
 
     async function getResponse() {
         try {
-            const response = await axios.get(
-                "https://testimonialapi.toolcarton.com/api"
-            );
+            const response = await axios.get(TESTIMONIES_API_URL);
             setTestimonies(response.data);
-            console.log(response.data);
+            const sortedTestimonies = response.data.sort(
+                (objA, objB) => Number(objB.rating) - Number(objA.rating)
+            );
+
+            setTestimonies(sortedTestimonies);
         } catch (error) {
             console.log(error);
         }
@@ -25,7 +29,9 @@ function App() {
             <h1>Testimonies</h1>
             <ul>
                 {testimonies?.map((testimony) => (
-                    <li key={testimony.id}>{testimony.name}</li>
+                    <li key={testimony.id}>
+                        {testimony.name} - &nbsp;<span>{testimony.rating}</span>
+                    </li>
                 ))}
             </ul>
         </div>
---
+++ src/api.js
@@ -0,0 +1,1 @@
+export const TESTIMONIES_API_URL = "https://testimonialapi.toolcarton.com/api";
